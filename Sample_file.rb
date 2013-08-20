21d8cd63a97b08f438628f26db5ce7bb6e0a3807
nithyak <knithya.sakthi@gmail.com>
2013-08-20 14:54:15 +0530

    Committing the changes made in the document.

---
+++ Sample_file.rb
@@ -0,0 +1,6 @@
+Sample_File
+Inserting into a new Sample File
+Checking for the git status command:
+"Changed but not updated"
+But the above status is not displayed..
+Need to find out when that status would be displayed.
